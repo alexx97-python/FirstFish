ee841a1c3f991083255183af08cdc45e49445339
Oleksii Sheiko <programmer.alex97@gmail.com>
2020-08-13 22:54:18 +0300

    Working with Stripe payment

--- core/views.py
+++ core/views.py
@@ -3,13 +3,17 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect, reverse
-from .models import Item, OrderItem, Order, BillingAddress, ItemImage
+from .models import Item, OrderItem, Order, BillingAddress, ItemImage, Payment
 from django.views.generic import ListView, DetailView, View, TemplateView
 from django.utils import timezone
 from .forms import CheckoutForm, ContactForm
 from django.core.mail import send_mail
 from myshop import settings
 from .filters import ItemFilter
+from django.conf import settings
+
+import stripe
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class CheckoutView(View):
@@ -55,8 +59,71 @@ class CheckoutView(View):
 
 
 class PaymentView(View):
+
     def get(self, *args, **kwargs):
-        return render(self.request, 'payment.html')
+        return render(self.request, 'core/payment.html')
+
+    def post(self, *args, **kwargs):
+        order = Order.objects.get(user=self.request.user, ordered=False)
+        token = self.request.POST.get('stripeToken')
+        amount = int(order.get_total() * 100)  # cents
+        try:
+            charge = stripe.Charge.create(
+                amount=amount,  #cents
+                currency="usd",
+                source=token,
+        )
+            # create the payment
+            payment = Payment()
+            payment.stripe_charge_id = charge['id']
+            payment.user = self.request.user
+            payment.amount = order.get_total()
+            payment.save()
+
+            # assign the payment to the order
+            order.ordered = True
+            order.payment = payment
+            order.save()
+            messages.success(self.request, 'The charge was successful!')
+            return redirect('core:home')
+
+        except stripe.error.CardError as e:
+            body = e.json_body
+            err = body.get('error', {})
+            messages.error(self.request, f'{err.get("message")}')
+            return redirect('core:home')
+
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            messages.error(self.request, 'Rate limit error')
+            return redirect('core:home')
+
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            messages.error(self.request, 'Invalid parameters')
+            return redirect('core:home')
+
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            messages.error(self.request, 'Not authenticated')
+            return redirect('core:home')
+
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            messages.error(self.request, 'Network error')
+            return redirect('core:home')
+
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            messages.error(self.request, 'Something went wrong.'
+                                         'You were not charged. Please try again.')
+            return redirect('core:home')
+
+        except Exception as e:
+            messages.error(self.request, 'A serious error occurred. We have been notified.')
+            return redirect('core:home')
 
 
 class HomeView(ListView):
--- myshop/settings.py
+++ myshop/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=True)
